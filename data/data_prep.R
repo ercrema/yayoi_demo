4ea9758d958a10af1982710ba6b0ecdc780f5ec2
Enrico R. Crema <enrico.crema@gmail.com>
2021-05-25 14:35:35 +0100

    Added C14 data and basic cleaning script

---
+++ data/data_prep.R
@@ -0,0 +1,19 @@
+library(here)
+library(rcarbon)
+
+# General Settings and Parameters:
+riceGrep = c("コメ","米","イネ")
+anthropicGrep = c("住居","埋葬","竪穴建物","掘立柱","墓","包含層","土坑","ピット","土器","捨場","遺構","炉","人骨","木舟","住","柱","Pit","焼土","カマド","床面","溝中","溝底部","建物跡","木製品","埋土","水田","竪坑","羨道","集石","漆器")
+
+
+# Read 14C Data ----
+load(here('data','japanc14db_v03.0(210218).RData')) #version 3.0
+
+# Cleaning ----
+# Consider only dates with Retain=TRUE
+c14db = subset(c14db,retain==TRUE)
+# Add Booleans fields
+c14db$anthropic = grepl(paste(anthropicGrep,collapse="|"),c14db$SamplingLocation) #sum(c14db$anthropic) 10683
+c14db$rice = grepl(paste(riceGrep,collapse="|"),c14db$MaterialType) #sum(c14db$rice) 398
+
+save(c14db,file=here('data','c14data.RData'))
