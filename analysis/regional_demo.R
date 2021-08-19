890c1e36b66cabc70b6d60b3e6ec6b7b7955db09
Enrico R. Crema <enrico.crema@gmai.com>
2021-08-19 18:24:05 +0100

    Updated regional demo to compare yayoi vs 1st 1000 yrs since farming

--- analysis/regional_demo.R
+++ analysis/regional_demo.R
@@ -6,90 +6,93 @@ library(parallel)
 
 # Load Data ----
 load(here('data','c14data.RData'))
-c14db = subset(c14db,C14Age<4000 & C14Age> 300 &  Material == 'Terrestrial') |> select(C14Age,C14Error,SiteID,Region=Region3,RegionID=RegionID3) |> arrange(RegionID)
+c14db = subset(c14db,C14Age<4000 & C14Age> 300 &  Material == 'Terrestrial' & anthropic == TRUE) |> select(C14Age,C14Error,SiteID,Region=Region3,RegionID=RegionID3) |> arrange(RegionID)
 
 
 # Site Level Thinning ----
 calibrated.dates <- calibrate(c14db$C14Age,c14db$C14Error)
-bin50  <- binPrep(sites = c14db$SiteID,ages=calibrated.dates,h=50)
-i  <- thinDates(ages=c14db$C14Age,  errors=c14db$C14Error, bins=bin50, size=1, thresh=1,seed=123,method='splitsample')
+bin  <- binPrep(sites = c14db$SiteID,ages=calibrated.dates,h=50)
+i  <- thinDates(ages=c14db$C14Age,  errors=c14db$C14Error, bins=bin, size=1, thresh=1,seed=123,method='splitsample')
 c14db  <-  c14db[i,]
 calibrated.dates <- calibrated.dates[i]
 
 # Setup Regions ----
 regionStartDates = unique(select(c14db,Region,RegionID)) |> arrange(RegionID)
-# regionStartDates$a = 3000
-regionStartDates$a = BCADtoBP(c(-1283,-1117,-981,-833,-685,-515,-362)) # 1283, 1117, 981, 833, 685, 515, 362
-regionStartDates$b500 = regionStartDates$a - 500
-regionStartDates$b1000 = regionStartDates$a - 1000
+regionStartDates$a1 = BCADtoBP(c(-1283,-1117,-981,-833,-685,-515,-362)) # 1283, 1117, 981, 833, 685, 515, 362
+regionStartDates$a2 = BCADtoBP(c(-900))
+regionStartDates$b1 = regionStartDates$a1 - 1000
+regionStartDates$b2 = BCADtoBP(250)
 
 # Subset Data to Relevant Temporal Window of Analyses ----
 c14db$med = medCal(calibrated.dates)
-c14db$r500 = NA
-c14db$r1000 = NA
+c14db$r1 = NA
+c14db$r2 = NA
 
 for (i in 1:nrow(regionStartDates))
 {
-	a = regionStartDates$a[i]
-	b500 = regionStartDates$b500[i]
-	b1000 = regionStartDates$b1000[i]
+	a1 = regionStartDates$a1[i]
+	a2 = regionStartDates$a2[i]
+	b1 = regionStartDates$b1[i]
+	b2 = regionStartDates$b2[i]
 	ii = which(c14db$RegionID == i)
-	c14db$r500[ii] = FALSE
-	c14db$r1000[ii] = FALSE
+	c14db$r1[ii] = FALSE
+	c14db$r2[ii] = FALSE
 	regionalDates = calibrated.dates[ii]
-	iii = ii[which.CalDates(regionalDates,BP < a & BP > b500,p=0.5)]
-	c14db$r500[iii] = TRUE
-	iii = ii[which.CalDates(regionalDates,BP < a & BP > b1000,p=0.5)]
-	c14db$r1000[iii] = TRUE
+	iii = ii[which.CalDates(regionalDates,BP < a1 & BP > b1,p=0.5)]
+	c14db$r1[iii] = TRUE
+	iii = ii[which.CalDates(regionalDates,BP < a2 & BP > b2,p=0.5)]
+	c14db$r2[iii] = TRUE
 }
 
 # Define Data & Constants ----
 data(intcal20)
-c14db500 = subset(c14db,r500==TRUE)
-c14db1000 = subset(c14db,r1000==TRUE)
+c14db1 = subset(c14db,r1==TRUE)
+c14db2 = subset(c14db,r2==TRUE)
 
 #Constants
-const500 <- const1000 <- list(calBP=intcal20$CalBP,C14BP=intcal20$C14Age,C14err=intcal20$C14Age.sigma)
-const500$Nregions <- const1000$Nregions <- max(c14db$RegionID)
-const500$Ndates <- nrow(c14db500)
-const1000$Ndates <- nrow(c14db1000)
-const500$region.id <- c14db500$RegionID
-const1000$region.id <- c14db1000$RegionID
-const500$starts <- const1000$starts <- regionStartDates$a
-const500$ends <- regionStartDates$b500
-const1000$ends <- regionStartDates$b1000
+const1 <- const2 <- list(calBP=intcal20$CalBP,C14BP=intcal20$C14Age,C14err=intcal20$C14Age.sigma)
+const1$Nregions <- const2$Nregions <- max(c14db$RegionID)
+const1$Ndates <- nrow(c14db1)
+const2$Ndates <- nrow(c14db2)
+const1$region.id <- c14db1$RegionID
+const2$region.id <- c14db2$RegionID
+const1$starts <- regionStartDates$a1
+const2$starts <- regionStartDates$a2
+const1$ends <- regionStartDates$b1
+const2$ends <- regionStartDates$b2
 
 # Data
-d500 <- list(cra=c14db500$C14Age,cra_error=c14db500$C14Error)
-d1000 <- list(cra=c14db1000$C14Age,cra_error=c14db1000$C14Error)
+d1 <- list(cra=c14db1$C14Age,cra_error=c14db1$C14Error)
+d2 <- list(cra=c14db2$C14Age,cra_error=c14db2$C14Error)
 
 # Define Sensible initialisation values for theta ----
-c14db500 <- left_join(c14db500,regionStartDates)
-c14db1000 <- left_join(c14db1000,regionStartDates)
-c14db500$theta.init <- c14db500$med
-c14db1000$theta.init <- c14db1000$med
-c14db500$theta.init[which(c14db500$theta.init>=c14db500$a)]  <- c14db500$a[which(c14db500$theta.init>=c14db500$a)] - 1
-c14db500$theta.init[which(c14db500$theta.init<=c14db500$b500)] <- c14db500$b[which(c14db500$theta.init<=c14db500$b500)] + 1
-c14db1000$theta.init[which(c14db1000$theta.init>=c14db1000$a)]  <- c14db1000$a[which(c14db1000$theta.init>=c14db1000$a)] - 1
-c14db1000$theta.init[which(c14db1000$theta.init<=c14db1000$b1000)] <- c14db1000$b[which(c14db1000$theta.init<=c14db1000$b1000)] + 1
-theta.init.500 <- c14db500$theta.init
-theta.init.1000  <- c14db1000$theta.init
+c14db1 <- left_join(c14db1,regionStartDates)
+c14db2 <- left_join(c14db2,regionStartDates)
+c14db1$theta.init <- c14db1$med
+c14db2$theta.init <- c14db2$med
+c14db1$theta.init[which(c14db1$theta.init>=c14db1$a1)]  <- c14db1$a1[which(c14db1$theta.init>=c14db1$a1)] - 1
+c14db1$theta.init[which(c14db1$theta.init<=c14db1$b1)] <- c14db1$b1[which(c14db1$theta.init<=c14db1$b1)] + 1
+c14db2$theta.init[which(c14db2$theta.init>=c14db2$a)]  <- c14db2$a2[which(c14db2$theta.init>=c14db2$a2)] - 1
+c14db2$theta.init[which(c14db2$theta.init<=c14db2$b2)] <- c14db2$b2[which(c14db2$theta.init<=c14db2$b2)] + 1
+theta.init.1 <- c14db1$theta.init
+theta.init.2  <- c14db2$theta.init
 
 # Sanity Check Plot
- spd.500 <- spd.1000 <- vector('list',length=nrow(regionStartDates))
+ spd1 <- spd2 <- vector('list',length=nrow(regionStartDates))
  for (i in 1:nrow(regionStartDates))
  {
- 	ii <- which(c14db500$RegionID==i)
- 	cal.500 <- calibrate(c14db500$C14Age[ii],c14db500$C14Error[ii],normalised=F)
- 	spd.500[[i]] <- spd(cal.500,timeRange=c(regionStartDates$a[i],regionStartDates$b500[i]))
+ 	ii <- which(c14db1$RegionID==i)
+ 	cal.1 <- calibrate(c14db1$C14Age[ii],c14db1$C14Error[ii],normalised=T)
+ 	spd1[[i]] <- spd(cal.1,timeRange=c(regionStartDates$a1[i],regionStartDates$b1[i]))
  
- 	ii <- which(c14db1000$RegionID==i)
- 	cal.1000 <- calibrate(c14db1000$C14Age[ii],c14db1000$C14Error[ii],normalised=F)
- 	spd.1000[[i]] <- spd(cal.1000,timeRange=c(regionStartDates$a[i],regionStartDates$b1000[i]))
+ 	ii <- which(c14db2$RegionID==i)
+ 	cal.2 <- calibrate(c14db2$C14Age[ii],c14db2$C14Error[ii],normalised=T)
+ 	spd2[[i]] <- spd(cal.2,timeRange=c(regionStartDates$a2[i],regionStartDates$b2[i]))
  }
  
- par(mfrow=c(3,3))
- lapply(spd.1000,plot)
+ par(mfrow=c(7,1),mar=c(4,1,1,1))
+ lapply(spd2,plot)
+ lapply(spd1,plot)
 
 
 
@@ -148,13 +151,13 @@ niter  <- 20000
 nburnin  <- 10000
 thin  <- 1
 
-chain_output_500 = parLapply(cl = cl, X = seeds, fun = runFun, d = d500,constants = const500, theta = theta.init.500, niter = niter, nburnin = nburnin,thin = thin)
-chain_output_1000 = parLapply(cl = cl, X = seeds, fun = runFun, d = d1000,constants = const1000, theta = theta.init.1000, niter = niter, nburnin = nburnin,thin = thin)
+chain_output_1000yrs = parLapply(cl = cl, X = seeds, fun = runFun, d = d1,constants = const1, theta = theta.init.1, niter = niter, nburnin = nburnin,thin = thin)
+chain_output_yayoi = parLapply(cl = cl, X = seeds, fun = runFun, d = d2,constants = const2, theta = theta.init.2, niter = niter, nburnin = nburnin,thin = thin)
 stopCluster(cl)
-post.sample500=coda::mcmc.list(chain_output_500)
-post.sample1000=coda::mcmc.list(chain_output_1000)
-coda::gelman.diag(post.sample500)
-coda::gelman.diag(post.sample1000)
+post.sample.1000yrs=coda::mcmc.list(chain_output_1000yrs)
+post.sample.yayoi=coda::mcmc.list(chain_output_yayoi)
+coda::gelman.diag(post.sample.1000yrs)
+coda::gelman.diag(post.sample.yayoi)
 
-save(post.sample500,file=here('R_images','regdemo500.RData'))
-save(post.sample1000,file=here('R_images','regdemo1000.RData'))
+save(post.sample.1000yrs,file=here('R_images','regdemo_1000yrs.RData'))
+save(post.sample.yayoi,file=here('R_images','regdemo_yayoi.RData'))
