0a9861299fe0c49645d8fbc157bea0dd3ee71506
Enrico R. Crema <enrico.crema@gmai.com>
2023-06-01 21:42:33 +0100

    Added completed .R pipeline for population estimates

--- analysis/pop_dens_est.R
+++ analysis/pop_dens_est.R
@@ -2,12 +2,6 @@
 library(here)
 library(dplyr)
 
-# x  <- read.csv(here('data','prefecture_region_match.csv'))
-# y  <- read.csv(here('data','prefectures_translations.csv'))
-# xy  <- left_join(x,y,by=c('Prefecture'='Translation')) |> subset(!Region.x%in%c('Hokkaido','Okinawa'))
-# xy  <- select(xy,PrefectureEn=Prefecture,JpNames,Region=Region.x,Region.koyama78=Region.alt,Area=Area.x,Surface=Area.y,Yayoi.start=Yayoi.start)
-# write.csv(xy,row.names=FALSE,file=here('data','prefecture_data.csv'))
-
 # Read External Data and Define Constants ----
 # Settlement size ratio for Yayoi, as discussed in Koyama 1978
 C.yayoi.haji  <- 1/3
@@ -15,7 +9,7 @@ C.yayoi.haji  <- 1/3
 koyama84  <- read.csv(here('data','koyama_popestimate_1984.csv'))
 koyama84  <- koyama84[-10,] #remove last row with total
 
-# Number of Archaeological Sites from https://www.bunka.go.jp/seisaku/bunkazai/shokai/pdf/maizobunkazai.pdf, table 1, page 27
+# Number of Archaeological Sites from https://www.bunka.go.jp/seisaku/bunkazai/shokai/pdf/h29_03_maizotokei.pdf, page 34
 maizobunkazai2017  <- read.csv(here('data','maizobunkazai_2017.csv')) |> subset(!Prefecture%in%c('北海道','沖縄県','合計'))
 
 
@@ -33,24 +27,23 @@ sitecounts$PhSh  <- sitecounts$Haji.pop/sitecounts$Haji.sites
 
 #Method 1: Koyama 1978
 Ph.Sh.kanto  <- subset(sitecounts,Region=='Kanto')$PhSh
-sitecounts$Yayoi.pop.est1  <- Ph.Sh.kanto * C.yayoi.haji * sitecounts$Yayoi.sites
+sitecounts$Yayoi.pop.est1  <- round(Ph.Sh.kanto * C.yayoi.haji * sitecounts$Yayoi.sites)
 
 #Method 2: Variable Sampling Fraction
-sitecounts$Yayoi.pop.est2  <- sitecounts$PhSh * C.yayoi.haji * sitecounts$Yayoi.sites
+sitecounts$Yayoi.pop.est2  <- round(sitecounts$PhSh * C.yayoi.haji * sitecounts$Yayoi.sites)
 
 #Method 3: Variable Sampling Fraction and Duration
 prefect.counts  <- select(maizobunkazai2017,Prefecture=PrefectureEn,Region.koyama78,Yayoi.sites=弥生,Surface,Area,Yayoi.start)
 prefect.counts  <- left_join(prefect.counts,select(sitecounts,Region,PhSh),by=c('Region.koyama78'='Region'))
-prefect.counts$Yayoi.pop.est3  <- prefect.counts$PhSh * C.yayoi.haji * prefect.counts$Yayoi.sites * 1000/abs(prefect.counts$Yayoi.start - 250)
+prefect.counts$Yayoi.pop.est3  <- round(prefect.counts$PhSh * C.yayoi.haji * prefect.counts$Yayoi.sites * 1000/abs(prefect.counts$Yayoi.start - 250))
 sitecounts  <- left_join(sitecounts,aggregate(Yayoi.pop.est3 ~ Region.koyama78, sum, data=prefect.counts),by=c('Region'='Region.koyama78'))
 
-# barplot(t(as.matrix(sitecounts[,c(2,6,7,9)])),beside=T,names.arg=sitecounts$Region)
 
 # Estimate based on Crema et al 2022 Regions ----
 estimates  <- group_by(prefect.counts,Area) |> summarise(Surface=sum(Surface),EstPop=sum(Yayoi.pop.est3)) |> as.data.frame()
-estimates$PopDens  <- estimates$EstPop/estimates$Surface
+estimates$PopDens  <- round(estimates$EstPop/estimates$Surface,2)
 
 
-
-
-
+# Store outputs ----
+write.csv(sitecounts,here('results','pop_estimate_compare.csv'),row.names=FALSE)
+write.csv(estimates,here('results','pop_estimate_region.csv'),row.names=FALSE)
