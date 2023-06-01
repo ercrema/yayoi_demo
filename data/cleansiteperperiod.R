7ee3dd7db59670bb8ffc292ab19ddd4839b352d4
Enrico R. Crema <enrico.crema@gmai.com>
2023-06-01 21:44:37 +0100

    Removed deprecated files

--- data/cleansiteperperiod.R
+++
@@ -1,7 +0,0 @@
-cleaning=read.csv("data/siteperpref.csv")
-
-remK <- function(i)gsub(",","",i)
-sinK <- apply(cleaning[,2:ncol(cleaning)],2,remK)
-cleaning[,2:ncol(cleaning)]=apply(sinK,2,as.numeric)
-write.csv(file="data/siteperprefperiod_cleaned.csv",cleaning,row.names=F)
-cleaned=read.csv("data/siteperperprefperiod_cleaned.csv")
